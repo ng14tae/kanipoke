32f257e1dcf9d3791879791fce84d4a61047e5b3
Tae JeongHwa <eh@TaenoMacBook-Air.local>
2025-08-26 22:29:58 +0900

    0826-tz

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -12,41 +12,15 @@ class BattlesController < ApplicationController
 
   def create
     @battle = Battle.new(battle_params)
-
-    # hiddenフィールドから受け取ったカード情報を設定
     @battle.user_card = params[:battle][:user_card].to_i
-    @battle.opponent_card = params[:battle][:opponent_card].to_i
 
-    retry_count = 0
-    max_retries = 3
-
-    # 決着がつくまで最大3回
-    max_retries.times do |i|
-    if i > 0
-      @battle.user_card = rand(1..13)
-      @battle.opponent_card = rand(1..13)
-    end
-
-    retry_count = i + 1
-
-    # 勝敗判定の呼び出し
-    winner_user = @battle.determine_winner
-    break if winner_user # 勝負がついたら終了
-    end
+    # ユーザーカードと異なる相手カードを確実に選択
+    available_cards = (1..13).to_a - [@battle.user_card]
+    @battle.opponent_card = available_cards.sample
 
     winner_user = @battle.determine_winner
-    message = "戦闘が完了しました！"
+    message = "戦闘が完了しました！#{winner_user.name}の勝利です！"
 
-    if winner_user.nil?
-      # 3回でも引き分け → ランダム決定
-      winner_user = [ @battle.user, @battle.opponent ].sample
-      message = "#{retry_count}回の引き分けの末、ランダムで#{winner_user.name}の勝利が決定しました！"
-    elsif retry_count > 1
-      # 再戦があった場合
-      message = "#{retry_count}回の激戦の末、#{winner_user.name}の勝利です！"
-    end
-
-    # 勝敗を設定
     @battle.winner_id = winner_user.id
     @battle.loser_id = winner_user == @battle.user ? @battle.opponent.id : @battle.user.id
 
