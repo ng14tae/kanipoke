bc997a2eac6400c3c52db1d251c4839602ab1aff
Tae JeongHwa <eh@TaenoMacBook-Air.local>
2025-08-27 21:56:55 +0900

    0827-dr

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -12,15 +12,22 @@ class BattlesController < ApplicationController
 
   def create
     @battle = Battle.new(battle_params)
+
+    # hiddenフィールドから受け取ったカード情報を設定
     @battle.user_card = params[:battle][:user_card].to_i
+    @battle.opponent_card = params[:battle][:opponent_card].to_i
 
-    # ユーザーカードと異なる相手カードを確実に選択
-    available_cards = (1..13).to_a - [@battle.user_card]
-    @battle.opponent_card = available_cards.sample
+    # 引き分けの場合はユーザーカードを変更（見えないので問題なし）
+    if @battle.user_card == @battle.opponent_card
+      available_user_cards = (1..13).to_a - [@battle.opponent_card]
+      @battle.user_card = available_user_cards.sample
+    end
 
+    # 勝敗判定（引き分けは発生しない）
     winner_user = @battle.determine_winner
     message = "戦闘が完了しました！#{winner_user.name}の勝利です！"
 
+    # 勝敗を設定
     @battle.winner_id = winner_user.id
     @battle.loser_id = winner_user == @battle.user ? @battle.opponent.id : @battle.user.id
 
